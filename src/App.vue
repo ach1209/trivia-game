2d1d48473df19e54acd2eec0d3b4fab705f926cc
Anthony <ach0074@gmail.com>
2022-10-20 10:27:33 -0400

    Created MainHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import "@fontsource/poppins"
 import MainHeader from '@/components/MainHeader.vue'
 </script>
 
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,12 +1,28 @@
-<script setup lang="ts"></script>
-
 <template>
   <header class="main-header">
     <h1 class="main-header-title">Trivia Game</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    <p class="main-header-info">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
   </header>
 </template>
 
 <style scoped>
+.main-header {
+  height: 15rem;
+  line-height: 4.5rem;
+  background-color: var(--primary);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
 
+.main-header-title {
+  color: var(--light);
+  font-size: 4rem;
+  font-weight: 700;
+}
+
+.main-header-info {
+  color: var(--light);
+}
 </style>
